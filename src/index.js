491e7eba430d11588e2d082ebf307f20e50ee3b6
walissonsou <walissonsouza96@hotmail.com>
2022-10-17 13:35:15 -0300

    Deve ser possível criar um cliente/conta

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@ const app = express();
 
 app.use(express.json())
 
+/* cpf - string
+name - string
+id - uuid
+statement - extrado que é um []
+*/
+
+app.post('/accounts', (req, res) => {
+  const { cpf, name } = request.body;
+
+ 
+})
 app.listen(3333)
