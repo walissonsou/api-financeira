4b940f8a6114893cd3b31cf2a742aeca18c12761
walissonsou <walissonsouza96@hotmail.com>
2022-10-18 09:54:56 -0300

    Deve ser possível realizar um saque

--- src/index.js
+++ src/index.js
@@ -17,6 +17,19 @@ function VerificaSeCpfJaExiste(req, res, next) {
   req.cliente = cliente;
   return next();
 }
+function buscarSaldo(extrato) {
+
+                              // acumulador e objeto que quer interar 
+  const saldo = extrato.reduce((acc, operationDeposit) => {
+    if (operationDeposit.type === "credito") {
+      return acc + operationDeposit.amount;
+    } else {
+      return acc - operationDeposit.amount;
+    }
+  }, 0);
+
+  return saldo;
+}
 app.post("/contas", (req, res) => {
   const { cpf, name } = req.body;
   const id = uuidv4();
@@ -47,16 +60,38 @@ app.post("/deposito", VerificaSeCpfJaExiste, (req, res) => {
     descricao,
     quantia,
     created_at: new Date(),
-    type: "credit",
+    type: "credito",
   };
 
   cliente.extrato.push(operationDeposit);
 
   return res.status(201).json(`Depósito de ${quantia} feito`);
 });
-app.get("/extrato/", VerificaSeCpfJaExiste, (req, res) => {
+app.get("/extrato", VerificaSeCpfJaExiste, (req, res) => {
   const { cliente } = req;
 
   return res.json(cliente.extrato);
 });
+app.post("/saque", VerificaSeCpfJaExiste, (req, res) => {
+  const { quantia } = req.body; // o que vai ser passado
+
+  const { cliente } = req; // busca o cliente pelo cpf pela request do header 
+
+  const saldo = buscarSaldo(cliente.extrato);
+
+  if (saldo < quantia) {
+    return res.status(400).json({ error: "Saldo insuficiente" });
+  }
+  const operationDeposit = {
+    quantia,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  cliente.extrato.push(operationDeposit);
+
+  return res.status(201).send();
+
+});
+
 app.listen(3333);
